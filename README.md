fec3c34ded448f9f320d043aede9ebffb9268798
Hardev Singh <hardev@frappedev.co.in>
2022-10-27 00:43:56 +0530

    added readme and updated script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# backup-aida2
- backup aida2 with python script
+# Backup AIDA2
+
+This python script is used to backup the codebase, along with the current resources.
+
+## Pre-requesties:
+
+1. Install required packages:
+    - gitpython
+    - python-dotenv
+    - cloudinary-cli
+    - aws-cli
+2. Configure Aws-cli:
+    - Run `aws configure`
+    - follow instructions
+3. Configure Cloudinary-cli:
+    - Run `export CLOUDINARY_URL={this can be found in cloudinary dashboard}`
+    - Check if it is configured by running `cld config`
+4. Copy .env.example to .env
+    - update with database credentials
+5. Now you can run the script by `python backup.py` and you will get a tar.gz file containing backup of everything.
--- backup.py
+++ backup.py
@@ -1,11 +1,29 @@
 from datetime import datetime
-from time import time
+import logging
 from dotenv import load_dotenv
 import os
 from git import Repo
 import dotenv
 
 load_dotenv()
+
+# create logger
+logger = logging.getLogger("logging_tryout2")
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter(
+    "%(asctime)s:  %(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S")
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
 
 BACKED_UP_FILE_NAME = (
     f"aida2_backup_{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}")
@@ -15,16 +33,8 @@ BACKUP_DIR = f"{CURRENT_FILE_DIR}/backup"
 CODEBASE_DIR = f"{BACKUP_DIR}/codebase"
 DOWNLOADS_DIR = f"{BACKUP_DIR}/downloads"
 
-# Pre-requesties
-# 1. first you need to install two cli tools cloudinary-cli and aws-cli
-# 2. both can be installed with pip3 install cloudinary-cli & pip3 install aws-cli
-# 3. later you need to configure those,
-# 4. for cloudinary, run `export CLOUDINARY_URL={can be found in cloudinary dashboard}`
-# 5. for aws-cli, run 'aws config'
-# 6. now you just need to add .env file
-# 7. if still didn't work, you might need to install python-dotenv and gitpython packages
-
 if os.path.exists(BACKUP_DIR):
+    logger.info('Deleting backup folder')
     os.system(f"rm -r {BACKUP_DIR}")
 else:
     os.makedirs(BACKUP_DIR)
@@ -36,27 +46,33 @@ os.makedirs(f"{DOWNLOADS_DIR}/s3", exist_ok=True)
 
 
 # 1. clone repo (move app and api folder into backup/codebase)
-Repo.clone_from('https://github.com/singhhrpreet/aida-2.0.git', CODEBASE_DIR)
+logger.info(
+    'Cloning codebase, please wait, it takes time, we are working on big project! 😉')
+# Repo.clone_from('https://github.com/singhhrpreet/aida-2.0.git', CODEBASE_DIR)
 
 # 2. export sql database and move file into backup/sql_export
-load_dotenv()
-
-exportCmd = f"mysqldump -h {dotenv.get_key('./.env', 'DB_HOST')} -u {dotenv.get_key('./.env', 'DB_USER')} -p{dotenv.get_key('./.env', 'DB_PASSWORD')} {dotenv.get_key('./.env', 'DB_DATABASE')} > ./backup/something.sql"
-print(exportCmd)
-os.system(exportCmd)
+logger.info('Exporting Database')
+export_cmd = f"mysqldump -h {dotenv.get_key('./.env', 'DB_HOST')} -u {dotenv.get_key('./.env', 'DB_USER')} -p{dotenv.get_key('./.env', 'DB_PASSWORD')} {dotenv.get_key('./.env', 'DB_DATABASE')} > ./backup/database.sql"
+os.system(export_cmd)
 
 # 3. backup files from cloudinary and S3 bucket.
-print(os.path.abspath(__file__))
-
+logger.info('Backing up cloudinary resources')
 cloudinary_dir = f"{DOWNLOADS_DIR}/cloudinary"
-os.chdir(cloudinary_dir)
-os.system(f"cld sync --pull {cloudinary_dir} shanikas")
 
+os.chdir(cloudinary_dir)
+
+remote_cloudinary_dir = input(
+    'Enter cloudinary directory name(if any, else whole cloudinary account resources will be downloaded): ') or '/'
+os.system(f"cld sync --pull {cloudinary_dir} {remote_cloudinary_dir}")
+
+logger.info('Backing up S3 bucket')
 s3_dir = f"{DOWNLOADS_DIR}/s3"
 os.chdir(s3_dir)
 os.system(f"aws s3 sync s3://hardevbucketsdf {s3_dir}")
 
+logger.info('Generating zip file')
 os.chdir(CURRENT_FILE_DIR)
 os.system(f"tar -czvf {BACKED_UP_FILE_NAME}.tar.gz backup")
 
 os.system("rm -r backup")
+logger.info('Backup complete!')
